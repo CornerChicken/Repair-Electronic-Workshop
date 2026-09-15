1886df9bf2637200f36136f8eb4911254af065cf
bouncinglizards <callister.misquitta@gmail.com>
2026-09-14 19:14:36 -0700

    Completed networking

--- Packages/replecs.lua
+++
@@ -1,5 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["pepeeltoro41_replecs@0.0.2"]["replecs"])
-export type Server = REQUIRED_MODULE.Server 
-export type Client = REQUIRED_MODULE.Client 
-export type ReplecsLib = REQUIRED_MODULE.ReplecsLib 
-return REQUIRED_MODULE
