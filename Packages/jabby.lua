75bf8886035c34e05259295f6b6882dafba62fbe
bouncinglizards <callister.misquitta@gmail.com>
2026-09-15 13:10:10 -0700

    Updated packages to fix compatability problems

--- Packages/jabby.lua
+++ Packages/jabby.lua
@@ -1,1 +1,1 @@
-return require(script.Parent._Index["alicesaidhi_jabby@0.2.3"]["jabby"])
+return require(script.Parent._Index["alicesaidhi_jabby@0.6.1"]["jabby"])
---
+++ Packages/replecs.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["pepeeltoro41_replecs@0.4.0-rc.3"]["replecs"])
